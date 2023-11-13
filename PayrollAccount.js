c8157821c91822243f71dd43db7c817b84cee494
Gill, Nick <Nick.Gill@ais.com>
2023-11-13 08:29:20 -0500

    working on create payroll account

---
+++ PayrollAccount.js
@@ -0,0 +1,23 @@
+// Inline Page
+var pageInput = {
+    pageType: "custom",
+    name: "ais_createpayrollaccount_bbd5e",
+    entityName: "<logical name of the table>",
+    recordId: "<record id>",
+};
+var navigationOptions = {
+    target: 2,
+    position: 1,
+    width: {value: 50, unit: "%"},
+    title: "Create Payroll Account"
+};
+Xrm.Navigation.navigateTo(pageInput, navigationOptions)
+    .then(
+        function () {
+            // Called when page opens
+        }
+    ).catch(
+        function (error) {
+            // Handle error
+        }
+    );
