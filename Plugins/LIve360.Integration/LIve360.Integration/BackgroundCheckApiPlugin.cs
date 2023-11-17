712221f2fe3b66cd7e9cfeabf5ec437a9ef56e4a
Tom McKearney <tom.mckearney@ais.com>
2023-11-17 12:54:59 -0500

    adding plugin code

---
+++ Plugins/LIve360.Integration/LIve360.Integration/BackgroundCheckApiPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Live360.Plugins
+{
+  public class BackgroundCheckApiPlugin : IPlugin
+  {
+    public void Execute(IServiceProvider serviceProvider)
+    {
+      var tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+      // Obtain the execution context from the service provider.
+      var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+      var orgFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+
+      // create without the user uses the system user
+      var systemUserService = orgFactory.CreateOrganizationService(null);
+
+      // get inputs
+
+      var idString = context.InputParameters["BackgroundCheckID"]?.ToString();
+      var safeToHireString = context.InputParameters["IsSafeToHire"]?.ToString();
+
+      tracingService.Trace($"ID String: {idString}, SafeToHire? {safeToHireString}");
+
+      var id = Guid.Parse(idString);
+      bool isSafeToHire = safeToHireString.ToLower() == "true";
+
+      // create an "update" entity
+      var updateEntity = new Entity("ais_backgroundcheck")
+      {
+        Id = id
+      };
+
+      updateEntity.Attributes.Add("ais_issafe", isSafeToHire);
+
+      // save results to the DB
+      systemUserService.Update(updateEntity);
+    }
+  }
+}
